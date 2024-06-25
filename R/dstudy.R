b293a930aa630a8ebee908f581c8a234ff790beb
tclark23 <tclark23@nd.edu>
2024-06-25 14:05:20 -0400

    pushin everythin

--- R/dstudy.R
+++ R/dstudy.R
@@ -1,22 +1,38 @@
-dstudy2 <- function(data, col.scores, from, to, by, rounded = 3) {
+#' Execute a D-Study
+#'
+#' @param data A dataframe containing a column for Person, a column for Trial, and several columns for metrics.
+#' @param col.scores A column index or variable name denoting the column in the original dataframe that serves as the dependent variable.
+#' @param from The first number of trials to test.
+#' @param to The final number of trials to test.
+#' @param by The interval at which trial numbers should be tested.
+#' @param rounded The number of decimal places the reliability coefficients should be rounded to.
+#'
+#' @return A dataframe of reliability coefficients for the specified numbers of trials.
+#' @export
+#'
+#' @examples
+#' x <- data.frame(c(1,1,1,2,2,2,3,3,3), c(1,2,3,1,2,3,1,2,3), c(12, 14, 12, 22, 22, 19, 17, 22, 5))
+#' colnames(x) <- c("Person", "Trial", "Metric")
+#' dstudy(x, col.scores = "Metric", from = 1, to = 10, by = 1)
+dstudy <- function(data, col.scores, from, to, by, rounded = 3) {
   if (!is.na(as.integer(col.scores)) & col.scores == as.integer(col.scores)) {
     data.small <- data %>%
-      select(1:2, as.integer(col.scores))
+      dplyr::select(1:2, as.integer(col.scores))
 
     col.scores <- colnames(data)[col.scores]
   }
   else {
     data.small <- data %>%
-      select(1:2, col.scores)
+      dplyr::select(1:2, col.scores)
   }
 
   colnames(data.small)[3] <- "Measure"
   formula2 <- Measure ~ (1|Person) + (1|Trial)
-  comps <- data.frame(gstudy(data = data.small, formula2)$components)
+  comps <- data.frame(gtheory::gstudy(data = data.small, formula2)$components)
   y <- c("Person", "Trial", "Residual")
 
   comps <- comps %>%
-    slice(match(y, source))
+    dplyr::slice(match(y, source))
 
   from <- as.integer(from)
   to <- as.integer(to)
